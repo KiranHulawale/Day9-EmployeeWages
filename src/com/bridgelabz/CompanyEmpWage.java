4b9b959617952209b12fbf6ad49d078c7c5d826f
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-25 13:15:03 +0530

    Update CompanyEmpWage.java

--- src/com/bridgelabz/CompanyEmpWage.java
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -1,2 +1,17 @@
-package com.bridgelabz;public class CompanyEmpWage {
+package com.bridgelabz;
+
+public class CompanyEmpWage {
+    int totalEmpWage;
+
+    public CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+
+        System.out.println("CompanyName :\" + companyName\n" +
+                                          "+ \" WagesPerHr :\" + wagePerHr\n" +
+                                             "+ \" MaxWokingDays :\" + maxWorkingDays\n" +
+                                                "+ \" MaxWorkingHr :\" + maxWorkingHrs\n" +"");
+    }
+
+    public void setTotalEmployeeWage(int totalWage) {
+        this.totalEmpWage = totalEmpWage;
+    }
 }
