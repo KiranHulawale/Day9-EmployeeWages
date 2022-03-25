fbcc1b8f545aceb7ab8d945028333dbc8e81823b
Kiran <kiranhulawale123@gmail.com>
2022-03-25 12:46:32 +0530

    Added UC-12 to list multiple company wages

--- src/com/bridgelabz/CompanyEmpWage.java
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -2,9 +2,11 @@ package com.bridgelabz;
 
 public class CompanyEmpWage {
 
+   
     int totalEmpWage;
 
     public CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+
     }
 
     public void setTotalEmployeeWage(int totalWage) {
--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,14 +5,21 @@ public class EmployeeWage<companies, noOfCompanies> extends EmployeeWages {
         super(companyName, wagePerHour, workingDaysPerMonth, workingHoursPerMonth);
     }
 
+
+    public interface EmployeeWageBuilder {
+
+        void createWageBuilder(); // public and abstarct
+        int getTotalWage(String companyName);
+
+    }
+
     static CompanyEmpWage[] companies;
     //companies = new CompanyEmpWage[noOfCompanies];
-     int EmployeeWageComputation(noOfCompanies) {
-        super.WageComputation(noOfCompanies);
-        return 0;
+   //  int EmployeeWageComputation(noOfCompanies) {
+        //super.WageComputation(noOfCompanies);
+        //return 0;
     }
 
 //    public EmployeeWage(int i) {
 //        //super();
 //    }
-}
